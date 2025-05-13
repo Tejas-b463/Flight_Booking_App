df804e4fa054969fd6301afcdb0d0ebba1f4891a
Tejas-b463 <108131310+Tejas-b463@users.noreply.github.com>
2025-05-13 12:00:53 +0530

    reinstall Dependencies

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    base: './',
+    base: '/', // ✅ use this for proper Vercel deployment
     plugins: [react()],
 });
